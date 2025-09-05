2e0a3b6d75a60efeae55b355798ebfd6edd5b697
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-09-05 16:56:56 +0530

    fix:error for creating Url table

--- index.js
+++ index.js
@@ -1,37 +1,30 @@
 import 'dotenv/config';
-import express from 'express';
-import { QueryTypes } from 'sequelize'; // <-- ADD THIS IMPORT
+
+import express from "express";
 import sequelize from './config/database.js';
 import Url from './models/url.js';
-
 const app = express();
+
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('URL Shortener API is running!');
 });
 
-async function startServer() {
+
+
+async function startServer(){
   try {
     await sequelize.authenticate();
-    console.log('✅ Database connection has been established successfully.');
-
-    // ADD THIS NEW BLOCK TO CHECK THE VERSION
-    const [results] = await sequelize.query("SELECT version();", { type: QueryTypes.SELECT });
-    console.log('✅ Node.js is connected to Server Version:', results.version);
-    // END OF NEW BLOCK
-
+    console.log("Database connected");
     await Url.sync();
-    console.log('✅ "Urls" table has been synced.');
-
+    console.log("Database synced");
     app.listen(PORT, () => {
-      console.log(`🚀 Server is running on port ${PORT}`);
-    });
-  } catch (error)
- {
-    console.error('Unable to connect to the database:', error);
+        console.log(`Server is running on port ${PORT}`);
+});
+  } catch (error) {
+    console.log(error);
   }
 }
-
 startServer();
-
