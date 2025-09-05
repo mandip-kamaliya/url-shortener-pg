2e39a5f39db99e1fd500b250ca4fd87e2ca6f184
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-09-05 16:13:20 +0530

    startServer added

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-import dotenv from 'dotenv';
+import 'dotenv/config';
 
 import express from "express";
+import sequelize from './config/database';
 const app = express();
-dotenv.config();
+
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,6 +11,19 @@ app.get('/', (req, res) => {
   res.send('URL Shortener API is running!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+
+
+async function startServer(){
+  try {
+    await sequelize.authenticate();
+    console.log("Database connected");
+    await sequelize.sync();
+    console.log("Database synced");
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
 });
+  } catch (error) {
+    console.log(error);
+  }
+}
+startServer();
