3c5de7bbfc2c9be6dc6abe439ec36f48fa068cd7
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-09-08 14:02:25 +0530

    add url.js

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ import 'dotenv/config';
 import express from "express";
 import sequelize from './config/database.js';
 import Url from './models/url.js';
-import { nanoid } from 'nanoid';
-import validUrl from "valid-url";
+import router from './routes/url.js';
+
 
 const app = express();
 app.use(express.json());
+
+app.use("/",rou)
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,21 +17,7 @@ app.get('/', (req, res) => {
   res.send('URL Shortener API is running!');
 });
 
-app.get("/api/:shortCode",async (req,res)=>{
-    
-   try {
-        const url = await Url.findOne({where:{shortCode:req.params.shortCode}});
 
-     if(url){
-       return res.redirect(url.originalUrl);
-     }else{
-       res.status(404).json("Url not found!!");
-     }
-   } catch (error) {
-    console.error(error);
-    return res.status(500).json("Server Error");
-   }
-})
 
 async function startServer(){
   try {
@@ -46,26 +34,3 @@ async function startServer(){
 }
 startServer();
 
-app.post("/api/shorten",async (req,res)=>{
-   const {originalUrl} = req.body;
-   const Base_URL = process.env.Base_URL;
-    if (!validUrl.isUri(originalUrl)) {
-      return res.status(400).json("url is not valid");
-    }
-
-    try {
-      let url = await Url.findOne({where:{originalUrl}});
-      if(url){
-        return res.status(200).json(url);
-      }
-      const shortCode = nanoid(7);
-      url = await Url.create({
-        originalUrl,
-        shortCode
-      });
-      return res.status(201).json(url)
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json("Server Error!!");
-    }
-})
---
+++ routes/url.js
@@ -0,0 +1,49 @@
+import { Router } from "express";
+
+import Url from './models/url.js';
+import { nanoid } from 'nanoid';
+import validUrl from "valid-url";
+
+const router = Router();
+
+router.get("/api/:shortCode",async (req,res)=>{
+    
+   try {
+        const url = await Url.findOne({where:{shortCode:req.params.shortCode}});
+
+     if(url){
+       return res.redirect(url.originalUrl);
+     }else{
+       res.status(404).json("Url not found!!");
+     }
+   } catch (error) {
+    console.error(error);
+    return res.status(500).json("Server Error");
+   }
+})
+
+router.post("/api/shorten",async (req,res)=>{
+   const {originalUrl} = req.body;
+   const Base_URL = process.env.Base_URL;
+    if (!validUrl.isUri(originalUrl)) {
+      return res.status(400).json("url is not valid");
+    }
+
+    try {
+      let url = await Url.findOne({where:{originalUrl}});
+      if(url){
+        return res.status(200).json(url);
+      }
+      const shortCode = nanoid(7);
+      url = await Url.create({
+        originalUrl,
+        shortCode
+      });
+      return res.status(201).json(url)
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json("Server Error!!");
+    }
+})
+
+export default router;
